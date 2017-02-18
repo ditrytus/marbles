c0ea399289b02a6aaafd57a8760cbe97f64a3ff7
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-02-18 22:36:24 +0100

    Added a sprial track.

---
+++ Assets/Scripts/RestartOnTap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartOnTap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 && Input.GetTouch(0).tapCount == 1)
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+}
