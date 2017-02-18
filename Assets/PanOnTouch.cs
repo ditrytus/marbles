2b634f3b49bb6e7845059c1e075313d4a4b14aee
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-02-18 01:04:59 +0100

    Better funnel and pan camera on touch.

---
+++ Assets/PanOnTouch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UniRx;
+using System;
+
+public class PanOnTouch : MonoBehaviour {
+
+	public int touchIndex;
+
+	public Vector2? prevTouchPosition;
+
+	private Camera cam;
+
+	private IDisposable touch;
+
+	void Start () {
+		cam = GetComponent<Camera>();
+		
+		var oneTouch = Observable.EveryUpdate()
+			.Where(_ => Input.touchCount == 1)
+			.Select(_ => Input.GetTouch(0));
+
+		oneTouch
+			.Where(t => t.phase == TouchPhase.Began)
+			.Subscribe(__ => {
+				touch = Observable.EveryUpdate()
+					.Where(_ => Input.touchCount == 1)
+					.Select(_ => Input.GetTouch(0))
+					.Pairwise()
+					.Select(t => (Vector3)(t.Previous.position - t.Current.position) * cam.orthographicSize / cam.pixelHeight * 2f)
+					.Subscribe(p => {
+						transform.position += transform.TransformDirection(p);
+					});
+			});
+
+		oneTouch
+			.Where(t => t.phase == TouchPhase.Ended | t.phase == TouchPhase.Canceled)
+			.Subscribe(_ => touch.Dispose());
+	}
+}
