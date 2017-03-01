e83650bc740fba1f525a344fedaabd55af661913
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-03-01 21:55:31 +0100

    Nice pouring of marbles into bowl.

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class ForceField : RxBehaviour {
+	public Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		this.OnTriggerStayAsObservable()
+			.Subscribe(x => {
+				var body = x.GetComponent<Rigidbody>();
+				body.AddForce(direction, ForceMode.Force);
+			});
+	}
+}
---
+++ Assets/Scripts/RandomizePosition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class RandomizePosition : RxBehaviour {
+
+	public bool randomizeX = true;
+
+	public bool randomizeY = true;
+
+	public bool randomizeZ = true;
+
+	public float radius = 1.0f;
+
+	private Vector3 originalPosition;
+
+	void Start () {
+		originalPosition = transform.position;
+		
+		var sub1 = Observable.EveryUpdate()
+			.Subscribe(_ => {
+				var point = Random.insideUnitSphere * radius;
+
+				transform.position = new Vector3(
+					randomizeX ? originalPosition.x + point.x : transform.position.x,
+					randomizeY ? originalPosition.y + point.y : transform.position.y,
+					randomizeZ ? originalPosition.z + point.z : transform.position.z);
+			});
+
+		AddSubscriptions(sub1);
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+}
