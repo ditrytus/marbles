f7457bbc53f9aadda6674517f4041faf9b6b8144
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-03-06 22:01:11 +0100

    Script for object facing always other object.

---
+++ Assets/Scripts/FaceAt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class FaceAt : RxBehaviour {
+
+	public Transform observedObject;
+
+	void Start () {
+		var sub1 = Observable.EveryUpdate()
+			.Subscribe(_ => {
+				this.transform.LookAt(observedObject);
+				this.transform.forward = -this.transform.forward;
+			});
+
+		AddSubscriptions(sub1);
+	}
+}
