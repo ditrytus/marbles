ba75c119c0e6098a22e1fbaa112435b54e6b9071
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-03-06 23:08:57 +0100

    Instantiating marbles on touch.

---
+++ Assets/Scripts/InstantiateOnTap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UniRx;
+
+public class InstantiateOnTap : RxBehaviour {
+
+	public Camera camera;
+	
+	public GameObject prefab;
+
+	public LayerMask layer;
+
+	void Start () {
+		var sub1 = Observable.EveryUpdate()
+			.Where(_ => Input.touchCount > 0)
+			.Subscribe(_ => {
+				foreach(var touch in Input.touches)
+				{
+					if (touch.phase == TouchPhase.Began)
+					{
+						var ray = camera.ScreenPointToRay(touch.position);
+						RaycastHit hit;
+						if (Physics.Raycast(ray, out hit, float.MaxValue, layer.value))
+						{
+							Debug.Log(hit.collider.gameObject.name + " hit!");
+							Instantiate(prefab, hit.point, Random.rotation);
+						}
+					}
+				}
+			});
+
+		AddSubscriptions(sub1);
+	}
+}
