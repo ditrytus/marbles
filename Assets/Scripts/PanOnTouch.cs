2ebfc3d26d17070c22a907402dfff2b56a042cc2
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-03-08 23:11:15 +0100

    Reimplemented panning without UniRx.

--- Assets/Scripts/PanOnTouch.cs
+++ Assets/Scripts/PanOnTouch.cs
@@ -1,82 +1,62 @@
 ﻿using UnityEngine;
 using UniRx;
 using System;
+using System.Linq;
 
 public class PanOnTouch : RxBehaviour {
 
-	const int TouchIndex = 0;
-
 	public Camera cam;
 
-	private IDisposable touch;
-
 	private Vector3 velocity;
-
-	private IDisposable inertia;
 
 	public AxesFilter constraint = AxesFilter.All;
 
 	public float glide;
 
-	void Start () {
+	private bool isGliding = false;
 
-		var oneTouch = Observable.EveryUpdate()
-			.Where(_ => Input.touchCount > TouchIndex)
-			.Select(_ => Input.GetTouch(TouchIndex));
+	private float glidedTime = 0;
 
-		var sub1 = oneTouch
-			.Where(t => t.phase == TouchPhase.Began)
-			.Subscribe(__ => {
-				if (inertia != null)
-				{
-					inertia.Dispose();
-				}
-
-				touch = Observable.EveryUpdate()
-					.Where(_ => Input.touchCount > TouchIndex)
-					.Select(_ => Input.GetTouch(TouchIndex))
-					.Pairwise()
-					.Select(t => { 
-						var d = (Vector3)(t.Previous.position - t.Current.position) * cam.orthographicSize / cam.pixelHeight * 2f;
-
-						var c = d / Mathf.Sin(0.5f * Mathf.PI - Mathf.Deg2Rad * Vector3.Angle(cam.transform.up, Vector3.up));
-
-						return c;
-					})
-					.Select(d => new Vector3(
-						constraint.x ? 0.0f : d.x,
-						constraint.y ? 0.0f : d.y,
-						constraint.z ? 0.0f : d.z))
-					.Subscribe(v => {
-						velocity = v;
-						cam.transform.position += v;
-					});
-			});
-
-		var sub2 = oneTouch
-			.Where(t => t.phase == TouchPhase.Ended | t.phase == TouchPhase.Canceled)
-			.Subscribe(_ => {
-				touch.Dispose();
-
-				inertia = Observable.EveryUpdate()
-					.Select(__ => Time.smoothDeltaTime)
-					.Scan((t,d) => t + d)
-					.Select(time => Vector3.Lerp(velocity, Vector3.zero, time / glide))
-					.Subscribe(v => {
-						cam.transform.position += v;
-					});
-			});
-
-		AddSubscriptions(sub1, sub2);
-	}
-
-	void OnDisable()
+	void Update()
 	{
-		CancelSubscriptions();
-	}
+		if (Input.touchCount > 0)
+		{
+			isGliding = false;
+		}
 
-	void OnEnable()
-	{
-		Start();
+		if (Input.touchCount == 1)
+		{
+			var touch = Input.touches.First();
+			if (touch.phase == TouchPhase.Moved)
+			{
+				var d = (Vector3)(touch.deltaPosition) * cam.orthographicSize / cam.pixelHeight * 2f;
+
+				var c = d / Mathf.Sin(0.5f * Mathf.PI - Mathf.Deg2Rad * Vector3.Angle(cam.transform.up, Vector3.up));
+
+				velocity = new Vector3(
+					constraint.x ? 0.0f : d.x,
+					constraint.y ? 0.0f : d.y,
+					constraint.z ? 0.0f : d.z);
+				
+				cam.transform.position -= velocity;
+			}
+			else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+			{
+				isGliding = true;
+				glidedTime = 0;
+			}
+		}
+		
+		if (isGliding)
+		{
+			glidedTime += Time.deltaTime;
+
+			cam.transform.position -= Vector3.Lerp(velocity, Vector3.zero, glidedTime / glide);
+
+			if (glidedTime > glide)
+			{
+				isGliding = false;
+			}
+		}
 	}
 }
