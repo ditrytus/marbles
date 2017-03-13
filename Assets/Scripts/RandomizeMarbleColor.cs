57e948121983763412cdf55918307add921953bc
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-03-13 23:45:46 +0100

    Colorful marbles.

---
+++ Assets/Scripts/RandomizeMarbleColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RandomizeMarbleColor : MonoBehaviour {
+
+	void Start () {
+		var marbleColorController = GetComponent<MarbleColorController>();
+		if (marbleColorController != null)	
+		{
+			marbleColorController.SetColor(
+				(MarbleColor)Random.Range(0, System.Enum.GetValues(typeof(MarbleColor)).Length));
+		}	
+	}
+}
