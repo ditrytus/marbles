caafa448f244ec742e65af947e6cf6bf92462193
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-03-15 21:45:06 +0100

    Text counter aligned with dispenser bowl.

---
+++ Assets/AlignWithObjectInCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[ExecuteInEditMode]
+public class AlignWithObjectInCamera : MonoBehaviour {
+
+	public Transform target;
+
+	public Camera viewCamera;
+
+	public Vector2 offset;
+
+	void Start ()
+    {
+        Align();
+    }
+
+    private void Align()
+    {
+        if (viewCamera == null)
+        {
+            throw new InvalidOperationException("View camera is not set!");
+        }
+        var targetCameraPos = viewCamera.WorldToScreenPoint(target.position);
+        var trans = GetComponent<RectTransform>();
+        if (trans == null)
+        {
+            throw new InvalidOperationException("There is no RectTransform attached!");
+        }
+        trans.anchoredPosition = (Vector2)targetCameraPos + offset;
+    }
+
+    void Update ()
+	{
+		Align();
+	}
+}
