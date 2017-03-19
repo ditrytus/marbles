60402ff6bba0318fbb2883d0538fc1da93974d56
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-03-19 23:06:27 +0100

    Implemented filo element.

---
+++ Assets/FiloController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System.Linq;
+
+public class FiloController : RxBehaviour {
+
+	public int maxCount = 5;
+
+	public float upPositionAngle;
+
+	public float downPositionAngle;
+
+	public ContainerController container;
+
+	public float switchTime = 1.0f;
+
+	private enum FiloState
+	{
+		Gathering,
+		SettleBeforeSwitchingDown,
+		SwitchingDown,
+		Releasing,
+		SwitchingUp
+	}
+
+	private FiloState state = FiloState.Gathering;
+
+	private float startTime;
+
+
+
+	private float TimeRatio
+	{
+		get
+        {
+            return CurrentTime / switchTime;
+        }
+    }
+
+    private float CurrentTime
+    {
+		get
+		{
+			return Time.time - startTime;
+		}
+    }
+
+	private Vector3 eulerAngles;
+
+    void Start () {
+		var sub1 = container.content.ObserveCountChanged(notifyCurrentCount:true)
+			.Subscribe(count => {
+				if (state == FiloState.Gathering && count >= maxCount)
+				{
+					state = FiloState.SettleBeforeSwitchingDown;
+				}
+			});
+
+		var sub2 = container.content.ObserveCountChanged(notifyCurrentCount:true)
+			.Subscribe(count => {
+				if (count == 0 && state == FiloState.Releasing)
+				{
+					state = FiloState.SwitchingUp;
+					startTime = Time.time;
+				}
+			});
+
+		AddSubscriptions(sub1, sub2);
+	}
+
+	void FixedUpdate()
+	{
+		if (state == FiloState.SettleBeforeSwitchingDown)
+		{
+			if (container.content.All(item => item.GetComponent<Rigidbody>().IsSleeping()))
+			{
+				state = FiloState.SwitchingDown;
+				eulerAngles = transform.localEulerAngles;
+				startTime = Time.time;
+			}
+		}
+		if (state == FiloState.SwitchingDown)
+		{
+			SetRotation(Mathf.LerpAngle(upPositionAngle, downPositionAngle, TimeRatio));
+			if (CurrentTime >= switchTime)
+			{
+				state = FiloState.Releasing;
+			}
+		}
+		if (state == FiloState.SwitchingUp)
+		{
+			SetRotation(Mathf.Lerp(downPositionAngle, upPositionAngle, TimeRatio));
+			if (CurrentTime >= switchTime)
+			{
+				state = FiloState.Gathering;
+			}
+		}
+	}
+
+	private void SetRotation(float newXAngle)
+    {
+        transform.localEulerAngles = new Vector3(
+			newXAngle,
+			eulerAngles.y,
+			eulerAngles.z);
+    }
+
+	// private void SetRotation(float newZAngle)
+    // {
+    //     transform.localEulerAngles = new Vector3(
+	// 		transform.localRotation.eulerAngles.x,
+	// 		transform.localRotation.eulerAngles.y,
+	// 		newZAngle);
+    // }
+}
