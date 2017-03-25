51d7a5265c9f83acaa78fc420a275c902365ee8f
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-03-25 18:58:30 +0100

    Added a spiral lift.

---
+++ Assets/Scripts/PokeSleepingRigidbodies.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokeSleepingRigidbodies : MonoBehaviour {
+
+	public float force;
+
+	void OnTriggerStay(Collider other)
+	{
+        if (other.attachedRigidbody.IsSleeping())
+		{
+			other.attachedRigidbody.AddForce(Vector3.up * force);    
+		}
+    }
+}
