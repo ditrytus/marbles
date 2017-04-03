7bd8a12172f50b90715da3a9146392eb9454703b
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-04-03 02:25:41 +0200

    Camera rotates around clicked object.

---
+++ Assets/Scripts/SetPositionToRaycastHitOnMouseButtonDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPositionToRaycastHitOnMouseButtonDown : MonoBehaviour
+{
+	private const float MaxDistance = 100000.0f;
+	public int mouseButton = 1;
+
+	public Camera cam;
+
+	public LayerMask layerMask;
+
+	public QueryTriggerInteraction hitTrigger = QueryTriggerInteraction.Ignore;
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(mouseButton))
+		{
+			var ray = cam.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, MaxDistance, layerMask.value, hitTrigger))
+			{
+				transform.position = hit.point;
+			}
+		}
+	}
+}
