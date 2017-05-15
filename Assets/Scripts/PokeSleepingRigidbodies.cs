c4a93c49b37b6126575894dfc0c7cd04dd537333
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-05-15 21:41:28 +0200

    Add no sleep zone to funnel to prevent marbles from stucking in them.

--- Assets/Scripts/PokeSleepingRigidbodies.cs
+++ Assets/Scripts/PokeSleepingRigidbodies.cs
@@ -6,11 +6,13 @@ public class PokeSleepingRigidbodies : MonoBehaviour {
 
 	public float force;
 
+	public bool randomDirection = false;
+
 	void OnTriggerStay(Collider other)
 	{
         if (other.attachedRigidbody.IsSleeping())
 		{
-			other.attachedRigidbody.AddForce(Vector3.up * force);    
+			other.attachedRigidbody.AddForce((randomDirection ? Random.onUnitSphere : Vector3.up) * force);    
 		}
     }
 }
