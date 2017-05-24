146bf34d13465da76af3fbcc4d8f319c6c9d4357
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-05-24 22:26:46 +0200

    Added level in and out animation.

---
+++ Assets/Scripts/SetAnimatorTriggerOnKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetAnimatorTriggerOnKey : MonoBehaviour {
+
+	public Animator animator;
+
+	public KeyCode key;
+
+	public string triggerName;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(key))
+		{
+			animator.SetTrigger(triggerName);
+		}
+	}
+}
