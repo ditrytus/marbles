0ef6c89bafc111f701a314890ebde80ea775d2ba
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-05-24 20:51:52 +0200

    Nice background.

---
+++ Assets/Scripts/SkyboxTintColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxTintColor : MonoBehaviour {
+	
+	public Skybox skybox;
+
+	public Color tintColor;
+
+	private Color previousColor;
+	
+	void Update () {
+		if (previousColor != tintColor)
+		{
+			skybox.material.SetColor("_SkyTint", tintColor);
+			previousColor = tintColor;
+		}
+	}
+}
