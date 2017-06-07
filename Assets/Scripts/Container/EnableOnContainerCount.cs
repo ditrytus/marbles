cd2f647e49fb2bdc9baf20aae9a9568f0db2efbc
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-06-07 22:47:31 +0200

    Dispenser glows red when empty.

---
+++ Assets/Scripts/Container/EnableOnContainerCount.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class EnableOnContainerCount : RxBehaviour
+{
+	public int triggerCount;
+
+	public ContainerController container;
+
+	public GameObject objectToEnable;
+
+	void Start ()
+	{
+		var sub1 = container.content.ObserveCountChanged()
+			.Where(c => c == triggerCount)
+			.Subscribe(c => {
+				objectToEnable.Enable();
+			});
+
+		var sub2 = container.content.ObserveCountChanged()
+			.Where(c => c != triggerCount)
+			.Subscribe(c => {
+				objectToEnable.Disable();
+			});
+
+		AddSubscriptions(sub1, sub2);
+	}
+}
