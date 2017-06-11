e6237974983fcdbb3fa8ae00ac4080d862373641
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-06-11 22:45:30 +0200

    Animating red tint on dispenser counter when empty.

---
+++ Assets/Scripts/TintMaterial.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class TintMaterial : MonoBehaviour
+{
+	public Color color;
+
+	public Renderer[] renderers;
+
+	private MaterialPropertyBlock[] propBlocks;
+
+	public Color initialColor;
+
+	void Start ()
+	{
+		propBlocks = new MaterialPropertyBlock[renderers.Length];
+		for (int i=0; i<renderers.Length; i++)
+		{
+			propBlocks[i] = new MaterialPropertyBlock();
+			renderers[i].GetPropertyBlock(propBlocks[i]);
+		}
+	}
+	
+	void Update ()
+    {
+        SetTintColor(color);
+    }
+
+    private void SetTintColor(Color newColor)
+    {
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].GetPropertyBlock(propBlocks[i]);
+
+            propBlocks[i].SetColor("_Color", newColor);
+
+            renderers[i].SetPropertyBlock(propBlocks[i]);
+        }
+    }
+
+    void OnDisable()
+	{
+		SetTintColor(initialColor);
+	}
+}
