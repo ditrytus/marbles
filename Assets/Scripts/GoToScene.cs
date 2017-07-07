e2a753448d27f3fb0f7b344440358710b294e7d7
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-07-07 23:27:42 +0200

    Transitioning to another scene.

---
+++ Assets/Scripts/GoToScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+	public string sceneName;
+
+	public void Go()
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
---
+++ Assets/Scripts/GoToSceneButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToSceneButtonHandler : MonoBehaviour
+{
+	public Animator animator;
+
+	public string nextSceneName;
+
+	public LevelCompletedController levelCompletedController;
+
+	public string triggerName = "Exit";
+
+	public void OnClick()
+	{
+		Debug.Log("Go to scene: " + triggerName);
+		levelCompletedController.nextSceneName = nextSceneName;
+		animator.SetTrigger(triggerName);
+	}
+}
