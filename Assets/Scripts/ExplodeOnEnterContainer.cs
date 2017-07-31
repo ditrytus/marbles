7068060f6e4350701908996e93eb1db761b1dd7c
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-07-31 22:37:01 +0530

    Prototype of main menu.

---
+++ Assets/Scripts/ExplodeOnEnterContainer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+public class ExplodeOnEnterContainer : ContainerController
+{
+	public FloatRange delayRange;
+
+	public FloatRange wiggleTimeRange;
+
+	protected override IObservable<Collider> ProcessOnEnter(IObservable<Collider> onEnter)
+    {
+        var sub1 = onEnter
+            .Subscribe(c =>
+			{
+				var destComp = c.gameObject.GetComponent<DestroyOnWrongColor>();
+				destComp.delay = UnityEngine.Random.Range(delayRange.min, delayRange.max);
+				destComp.wiggleTime = UnityEngine.Random.Range(wiggleTimeRange.min, wiggleTimeRange.max);
+				c.gameObject.SendMessage(MarbleColorFilteredContainer.MarbleColorIsWrong);
+            });
+
+        AddSubscriptions(sub1);
+
+        return onEnter;
+    }
+}
---
+++ Assets/Scripts/SetAnimatorTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetAnimatorTrigger : MonoBehaviour {
+
+	public Animator animator;
+
+	public string triggerName;
+
+	public void SetTrigger()
+	{
+		animator.SetTrigger(triggerName);
+	}
+}
