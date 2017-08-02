e78d9d261439c83a679e5e800f6084a1883e5645
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-08-02 22:48:13 +0530

    Added a thumbnail for level 1.

---
+++ Assets/Scripts/CameraSnapshot.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+ 
+public class CameraSnapshot : MonoBehaviour
+{
+	public RenderTexture renderTexture;
+
+	public Camera renderCamera;
+
+	public string snapshotKey = "s";
+	
+	void LateUpdate()
+	{
+	
+		if (Input.GetKeyDown(snapshotKey))
+		{
+			StartCoroutine(SaveCameraView());
+		}
+	
+	}
+	
+	public IEnumerator SaveCameraView()
+    {
+        yield return new WaitForEndOfFrame();
+
+        RenderTexture rendText = RenderTexture.active;
+        RenderTexture.active = renderCamera.targetTexture;
+
+        renderCamera.Render();
+
+        Texture2D cameraImage = new Texture2D(renderCamera.targetTexture.width, renderCamera.targetTexture.height, TextureFormat.ARGB32, false);
+        cameraImage.ReadPixels(new Rect(0, 0, renderCamera.targetTexture.width, renderCamera.targetTexture.height), 0, 0);
+        cameraImage.Apply();
+        RenderTexture.active = rendText;
+
+		var path = GetSnapshotPath();
+		Debug.Log("Screenshot saved to: " + path);
+
+        File.WriteAllBytes(path, cameraImage.EncodeToPNG());
+    }
+
+    private string GetSnapshotPath()
+    {
+        var snapshotPath = String.Empty;
+        var snapshotNumber = 0;
+        do
+        {
+            snapshotNumber++;
+            snapshotPath = Path.Combine(
+				Application.persistentDataPath, 
+				"shot" + snapshotNumber.ToString("D4") + ".png");
+
+        } while (File.Exists(snapshotPath));
+        return snapshotPath;
+    }
+}
