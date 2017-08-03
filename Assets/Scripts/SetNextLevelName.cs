6a42ea33eaa1fa9c0bd7c4cc105565a5f232d35d
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-08-03 22:04:42 +0530

    Next level button automatically generates next level name.

---
+++ Assets/Scripts/SetNextLevelName.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+using System;
+
+public class SetNextLevelName : MonoBehaviour
+{
+	public Button button;
+
+	public GoToSceneButtonHandler handler;
+
+	void Start ()
+	{
+        button.onClick.AddListener((UnityAction)(() => handler.OnClick("Level" + (int.Parse(Regex.Match(SceneManager.GetActiveScene().name, @"(\d+)$").Groups[1].Value) + 1).ToString("D4"))));
+	}
+}
