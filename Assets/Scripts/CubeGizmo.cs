eea50a8fb51bb42778495862852ab56d8f22b835
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-08-20 20:21:14 +0530

    Level 8!

---
+++ Assets/Scripts/CubeGizmo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGizmo : MonoBehaviour
+{
+	public Color color = Color.yellow;
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = color;
+		Gizmos.DrawWireCube(this.transform.position, this.transform.localScale);
+	}
+}
