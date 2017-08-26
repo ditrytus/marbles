d2aea65922468e5a956412d21a8bd7f817e6aa67
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-08-26 19:38:24 +0530

    Refined level button prefab.

--- Assets/Scripts/LevelButtonStatusController.cs
+++ Assets/Scripts/LevelButtonStatusController.cs
@@ -38,6 +38,8 @@ public class LevelButtonStatusController : MonoBehaviour
 
 	public Text lockIcon;
 
+	public Button button;
+
 	public string defaultLockIconName = "LockIcon";
 
 	public StatesColors levelThumbColors = new StatesColors
@@ -59,11 +61,27 @@ public class LevelButtonStatusController : MonoBehaviour
 	{
 		if (levelThumb == null) levelThumb = gameObject.GetComponent<Image>();
 		if (lockIcon == null) lockIcon = transform.FindChild(defaultLockIconName).GetComponent<Text>();	
+		if (button == null) button = GetComponent<Button>();
 	}
 
 	public void SetButtonState(LevelButtonState state)
-	{
-		levelThumb.color = levelThumbColors.GetColorForState(state);
-		lockIcon.color = lockIconColors.GetColorForState(state);
-	}
+    {
+        levelThumb.color = levelThumbColors.GetColorForState(state);
+        lockIcon.color = lockIconColors.GetColorForState(state);
+        SetInteractable(state);
+    }
+
+    private void SetInteractable(LevelButtonState state)
+    {
+        switch (state)
+        {
+            case LevelButtonState.Locked:
+                button.interactable = false;
+                break;
+            case LevelButtonState.Hidden:
+            case LevelButtonState.Unlocked:
+                button.interactable = true;
+                break;
+        }
+    }
 }
