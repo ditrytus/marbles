04a51f04925894da0b021d4ee670f5cda485d39b
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-09-05 22:53:40 +0530

    Added sound of marble rolling on metal.

---
+++ Assets/Scenes/VelocityToVolume.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityToVolume : MonoBehaviour
+{
+	public AudioSource audioSource;
+
+	public FloatRange velocityRange = new FloatRange(){min = 0.0f, max = 1.0f};
+
+	public float vanishingIndex = 2.0f;
+
+	public float volumeFactor = 1.0f;
+
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (audioSource == null) audioSource = this.gameObject.GetComponent<AudioSource>();
+		body = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+	{
+		var v = body.velocity.magnitude;
+		var t = velocityRange.Lerpify(v);
+		var volume = Mathf.Pow(Mathf.Clamp(Mathf.Lerp(0.0f, 1.0f, t), 0.0f, 1.0f), vanishingIndex) * volumeFactor;
+
+		audioSource.volume = volume;
+	}
+}
---
+++ Assets/Scripts/FrictionSound.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrictionSound : MonoBehaviour
+{
+	public AudioSource audioSource;
+
+	public string tagFilter;
+
+	private bool isCollision;
+
+	private bool playsSound = false;
+	
+	void Start()
+	{
+		if (audioSource == null) audioSource = this.gameObject.GetComponent<AudioSource>();
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (!other.gameObject.CompareTag(tagFilter))
+		{
+			return;
+		}
+
+		isCollision = true;
+	}
+
+	void Update()
+	{
+		if (isCollision && !playsSound)
+		{
+			playsSound = true;
+			audioSource.Play();
+		}
+		if (!isCollision && playsSound)
+		{
+			playsSound = false;
+			audioSource.Stop();
+		}
+	}
+
+	void OnCollisionExit(Collision other)
+	{
+		if (!other.gameObject.CompareTag(tagFilter))
+		{
+			return;
+		}
+
+		isCollision = false;
+	}
+
+	void OnCollisionStay(Collision other)
+	{
+		if (!other.gameObject.CompareTag(tagFilter))
+		{
+			return;
+		}
+
+		if (!isCollision)
+		{
+			isCollision = true;			
+		}
+	}
+}
