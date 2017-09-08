2acafe7374bf91a34b4d3d0191ae269e32319b4c
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-09-08 23:25:16 +0530

    Increasing sound pitch when time is speed up.

--- Assets/Scripts/ComponentExtensions.cs
+++ Assets/Scripts/ComponentExtensions.cs
@@ -8,4 +8,9 @@ public static class ComponentExtensions
 	{
 		if (component == null) component = script.gameObject.GetComponent<TComponent>();
 	}
+
+	public static void SetDefaultToName(this MonoBehaviour script, ref GameObject gameObject, string defaultName)
+	{
+		if (gameObject == null) gameObject = GameObject.Find(defaultName);
+	}
 }
--- Assets/Scripts/TimeSpeedController.cs
+++ Assets/Scripts/TimeSpeedController.cs
@@ -2,17 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Linq;
 
 public class TimeSpeedController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 	public FloatRange timeScaleRange = new FloatRange(){min=3, max=15};
+
+    public GameObject rootWithAudio;
+
+    public string defaultRootName;
+
+    public float pitchFactor = 2.3f;
+
+    private Dictionary<AudioSource, float> audioSourcesWithPitch;
+
+    private bool isPressed = false;
+
+    void Start()
+    {
+        this.SetDefaultToName(ref rootWithAudio, defaultRootName); 
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (isPressed)
+        {
+            return;
+        }
+        isPressed = true;
         Time.timeScale = timeScaleRange.max;
+        IncreaseSoundPitch();
+    }
+
+    private void IncreaseSoundPitch()
+    {
+        audioSourcesWithPitch = rootWithAudio
+            .GetComponentsInChildren<AudioSource>()
+            .ToDictionary(a => a, a => a.pitch);       
+        foreach (var audioWithPitch in audioSourcesWithPitch)
+        {
+            Debug.Log(string.Format("Pitch from: {0} to: {1}", audioWithPitch.Key.pitch, audioWithPitch.Value * pitchFactor));
+            audioWithPitch.Key.pitch = audioWithPitch.Value * pitchFactor;
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        if (!isPressed)
+        {
+            return;
+        }
+        isPressed = false;
         Time.timeScale = timeScaleRange.min;
+        RestoreOriginalSoundPitch();
+    }
+
+    private void RestoreOriginalSoundPitch()
+    {
+        foreach (var audioWithPitch in audioSourcesWithPitch)
+        {
+            if (audioWithPitch.Key != null)
+            {
+                audioWithPitch.Key.pitch = audioWithPitch.Value;
+            }
+        }
     }
 }
