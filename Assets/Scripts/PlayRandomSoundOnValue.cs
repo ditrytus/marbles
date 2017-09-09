cdd341cab8e267c0ed1e547925c3954257ee87f7
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-09-09 21:18:45 +0530

    Added sound when countdown reaches zero.

---
+++ Assets/Scripts/PlayRandomSoundOnValue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomSoundOnValue : PlayRandomSoundBase {
+
+	public int triggerValue = 0;
+
+	public float volume = 1.0f;
+
+	public void CounterValueChanged(int value)
+	{
+		if (triggerValue == value)
+		{
+			PlayRandomSound(volume);
+		}
+	}
+}
