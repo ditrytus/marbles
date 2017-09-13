9343de72598137a818498a41cf196ed7bf37cb4f
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-09-13 21:25:21 +0530

    Added sounds for pause menu buttons.

---
+++ Assets/Scripts/BroadcastOnPointerDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BroadcastOnPointerDown : MonoBehaviour, IPointerDownHandler
+{	
+	public string message;
+
+	public SendMessageOptions sendMessageOptions = SendMessageOptions.DontRequireReceiver;
+
+	public void OnPointerDown(PointerEventData eventData)
+    {
+		Debug.Log("ButtonDown Sent");
+		SendMessageUpwards(message, sendMessageOptions);
+	}
+}
---
+++ Assets/Scripts/PlayAudioSourceOnPointerDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayAudioSourceOnPointerDown : MonoBehaviour
+{
+	public AudioSource audioSource;
+
+	void Start ()
+	{
+		this.SetDefaultFromThis(ref audioSource);
+	}
+	
+	public void ButtonDown()
+    {
+		Debug.Log("ButtonDown Received");
+		audioSource.Play();
+	}
+}
