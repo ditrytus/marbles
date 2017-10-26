88437af024b8ece97ce66494e052b0f27138583d
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-10-26 17:23:29 +0200

    Added analytics to levels.

---
+++ Assets/Scripts/TimeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeProvider : MonoBehaviour
+{
+	public float timeSinceLevelLoad;
+
+	public float framerate;
+
+	void Update ()
+	{
+		timeSinceLevelLoad = Time.timeSinceLevelLoad;
+		framerate = 1.0f / Time.deltaTime;
+	}
+}
