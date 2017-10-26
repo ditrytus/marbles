d8e23ff891263f9340317c218c9f2f261740f7fe
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-10-26 18:21:52 +0200

    Persisting level scroll position.

---
+++ Assets/Scripts/PersistentScrollState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+[Serializable]
+public class ScrollState
+{
+	public float horizontalNormalizedPosition;
+}
+
+public class PersistentScrollState : MonoBehaviour
+{
+	private const string ScrollStateFilePath = "menu_scroll.json";
+
+    private Persistent<ScrollState> scrollState;
+
+	public ScrollRect scrollRect;
+
+    void Start()
+    {
+		this.SetDefaultFromThis(ref scrollRect);
+        scrollState = new Persistent<ScrollState>(ScrollStateFilePath, () => new ScrollState() { horizontalNormalizedPosition = scrollRect.horizontalNormalizedPosition });
+		Debug.Log("LOAD HNP: " + scrollState.Subject.horizontalNormalizedPosition);
+		scrollRect.horizontalNormalizedPosition = scrollState.Subject.horizontalNormalizedPosition;
+    }
+
+	public void LevelSelected()
+	{
+		scrollState.Subject.horizontalNormalizedPosition = scrollRect.horizontalNormalizedPosition;
+		Debug.Log("SAVE HNP: " + scrollState.Subject.horizontalNormalizedPosition);
+		scrollState.Save();		
+	}
+}
---
+++ Assets/Scripts/SendMessageUpowardsButtonClickHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class SendMessageUpowardsButtonClickHandler : MonoBehaviour {
+
+	public Button button;
+
+	public string methodName;
+
+	public SendMessageOptions options;
+
+	void Start ()
+	{
+		this.SetDefaultFromThis(ref button);
+		button.onClick.AddListener((UnityAction)(() => this.SendMessageUpwards(methodName, options)));
+	}
+}
