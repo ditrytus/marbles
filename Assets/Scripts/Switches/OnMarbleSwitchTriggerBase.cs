3facccacbbaab08e4933e1ba9123697ddd3f0d54
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2017-11-26 19:50:29 +0100

    Added Level 13.

--- Assets/Scripts/Switches/OnMarbleSwitchTriggerBase.cs
+++ Assets/Scripts/Switches/OnMarbleSwitchTriggerBase.cs
@@ -8,15 +8,24 @@ public abstract class OnMarbleSwitchTriggerBase : SwitchTriggerBase
 {
 	private GameObject lastMarble;
 
+    public float lastMarbleTimeout = 3.0f;
+
 	protected void SwitchWithObject(GameObject triggeringObject)
     {
         if (triggeringObject != lastMarble && triggeringObject.CompareTag(Tags.Marble))
         {
             Trigger();
 
+            StopAllCoroutines();
             lastMarble = triggeringObject;
+            StartCoroutine(MarbleTimeout());
         }
     }
 
-    
+    IEnumerator MarbleTimeout()
+    {
+        yield return new WaitForSecondsRealtime(lastMarbleTimeout);
+        lastMarble = null;
+        yield break;
+    }
 }
