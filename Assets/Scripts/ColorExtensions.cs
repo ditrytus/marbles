ef7be3bdfacdd424f0befc1afaac88359bb587b8
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2018-01-02 16:46:10 +0100

    Drop funnels have random colors.

---
+++ Assets/Scripts/ColorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ColorExtensions
+{
+	public static Color ChangeBrightness(this Color color, float brightness)
+	{
+		float h, s, v;
+		Color.RGBToHSV(color, out h, out s, out v);
+		return Color.HSVToRGB(h, s, brightness);
+	}
+}
---
+++ Assets/Scripts/RandomEmission.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RandomEmission : MonoBehaviour
+{
+	public Color[] colorPool;
+
+	public float brightness;
+
+	private static List<int> usedColors;
+
+	void Start ()
+	{
+		if (usedColors == null)
+		{
+			usedColors = Enumerable.Range(0, colorPool.Length).ToList();
+		}
+
+		var renderer = GetComponent<Renderer>();
+
+		var randomIndex = Random.Range(0, usedColors.Count);
+
+		var randomColor = colorPool[usedColors[randomIndex]];
+
+		renderer.material.SetColor("_EmissionColor", randomColor.ChangeBrightness(brightness));
+
+		usedColors.RemoveAt(randomIndex);
+	}
+}
