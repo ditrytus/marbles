a5060b6affa649825f52c62d1ebfe54a8100b7de
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2018-01-09 22:30:22 +0100

    Level 23!!!!

---
+++ Assets/Scripts/CycleRotation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleRotation : MonoBehaviour
+{
+	public Vector3 angles;
+
+	public float initialShift;
+
+	private Vector3 initialAngles;
+
+	public float speed;
+
+	public float cycleInterval;
+
+    void Start ()
+	{
+		initialAngles = transform.rotation.eulerAngles;
+	}
+	
+	void Update ()
+	{
+		if (PausableTime.Instance.IsPaused)
+		{
+			return;
+		}
+
+		transform.rotation = Quaternion.Euler(initialAngles + angles * (Mathf.Sin((initialShift / speed + PausableTime.Instance.Time) * speed * 2 * Mathf.PI) + 1.0f)/2.0f);
+	}
+}
