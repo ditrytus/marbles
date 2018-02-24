6c9c8f1974467c088636e1e51de8171c5127c9b0
Jakub Gruszecki <gruszecki.jakub@gmail.com>
2018-02-24 20:11:35 +0100

    Fixing problem with marbles bumping on one another in Level 24.

---
+++ Assets/Scripts/ShiftToLayerOnTriggerEnter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShiftToLayerOnTriggerEnter : MonoBehaviour {
+
+	public string filterTag = "Marble";
+
+	public LayerMask onEnterLayer;
+	public LayerMask onExitLayer;
+
+	void OnTriggerEnter(Collider other)
+	{
+		SetLayerForFilter(other.gameObject, onEnterLayer.value);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		SetLayerForFilter(other.gameObject, onExitLayer.value);
+	}
+
+	void SetLayerForFilter(GameObject gObject, int layer)
+	{
+		if (gObject.CompareTag(filterTag))
+		{
+			gObject.layer = layer;
+		}
+	}
+}
